b6f3ab1d2efafbb56e368199560a409d605e85fc
Subrat Mallick <m.subrat198470@gmail.com>
2022-05-30 07:45:31 +0530

    ic

---
+++ js/randomlines.js
@@ -0,0 +1,44 @@
+{
+    {
+        const lib = [
+            "Aztec Col : First Volume",
+            ": )",
+        ];
+        ! function Newline() {
+            const randomLine = Math.floor(Math.random() * lib.length);
+            document.getElementById('barr').innerHTML = lib[randomLine];
+        }();
+    }
+    {
+        const lib2 = [
+            "Subbrat made this",
+            ": )",
+            "Happy Learning",
+            "This line changes",
+            "Thank me later",
+            "Why watching Title?",
+            "Focus on Tutorial",
+            "Aztec Col : First Volume",
+            "Dimensen",
+            "Dimensen by Subbrat",
+        ];
+        ! function Newline() {
+            const randomLine = Math.floor(Math.random() * lib2.length);
+            document.getElementById('tit').innerHTML = lib2[randomLine];
+        }();
+    }
+    {
+        const lib2 = [
+            "C Sharp",
+            " c#",
+        ];
+        ! function Newline() {
+            const randomLine = Math.floor(Math.random() * lib2.length);
+            document.getElementById('rtx1').innerHTML = lib2[randomLine];
+        }();
+    }
+
+}
+// function getRandomNumberBetween(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
