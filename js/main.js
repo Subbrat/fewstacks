5c05a30c39b43dae11a51709ac73d1011e69431b
Subrat Mallick <m.subrat198470@gmail.com>
2022-05-31 15:53:45 +0530

    h

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 {
     document.oncontextmenu = new Function("return false");
 }
+{
+    const countE0 = document.getElementById('ct0');
+    updateVisitCount();
+
+    function updateVisitCount() {
+        fetch('https://api.countapi.xyz/update/subbrat/experimente/?amount=+1')
+            .then(res => res.json())
+            .then(res => {
+                countE0.innerHTML = res.value;
+            })
+    }
+}
